def03b0e9500d8647b8e862a26573f47a64387bd
Deen Mohammad <dweenmohammed@gmail.com>
2021-06-23 13:02:07 +0600

    customers data insertion and csv file added

---
+++ Insertion queries/customers_insertion.sql
@@ -0,0 +1,19 @@
+USE online_shopping_DB;
+
+INSERT INTO customers(first_name, last_name, phone,email,birth_date)
+VALUES('deen', 'mohammad', 01796895816, 'dween@gmail.com', '1990-10-12');
+INSERT INTO customers(first_name, last_name, phone,email,birth_date)
+VALUES('alex', 'morgan', 01757417821, 'alex@gmail.com','1991-05-21');
+INSERT INTO customers(first_name, last_name, phone,email,birth_date)
+VALUES('steve', 'moris', 01721564186, 'steve@gmail.com', '1990-10-12');
+INSERT INTO customers(first_name, last_name, phone,email,birth_date)
+VALUES('virat', 'angshu', 018456218956, 'virat@gmail.com', '1985-10-12');
+INSERT INTO customers(first_name, last_name, phone,email,birth_date)
+VALUES('anupom', 'roy', 01565698456, 'roy@gmail.com', '1987-10-12');
+INSERT INTO customers(first_name, last_name, phone,email,birth_date)
+VALUES('eilysa', 'perry', 01688974786, 'perry@gmail.com', '1996-10-12');
+INSERT INTO customers(first_name, last_name, phone,email,birth_date)
+VALUES('nicholas', 'origi', 01712436816, 'nicholas@gmail.com', '1990-12-27');
+
+SELECT * 
+FROM customers;
